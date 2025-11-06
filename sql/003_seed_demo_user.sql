aaa76e70390fcb85f016c7ad336fd0e3372e813c
HostItSheet <harmonysheetsshop@gmail.com>
2025-11-06 13:00:13 +0000

    Delete sql/003_seed_demo_user.sql

--- sql/003_seed_demo_user.sql
+++
@@ -1,8 +0,0 @@
--- Seed a specific email/password account for manual testing.
--- Run this in the Supabase SQL editor with service-role permissions.
-select
-  auth.create_user(
-    email        => 'Josefsen.eivind@gmail.com',
-    password     => 'Admin123',
-    email_confirm => true
-  );
