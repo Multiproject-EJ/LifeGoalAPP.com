d13aa21d0b86322ba20dcf066199bd39282534f0
Multiproject-EJ <josefsen.eivind@gmail.com>
2026-06-14 21:24:18 +0200

    Limit WebP island background change to existing asset

--- src/features/gamification/level-worlds/services/islandBackgrounds.ts
+++ src/features/gamification/level-worlds/services/islandBackgrounds.ts
@@ -1,5 +1,12 @@
-import { getIslandArtFolderName } from './islandArtManifest';
+import { getIslandArtFolderName, normalizeIslandArtIslandNumber } from './islandArtManifest';
+
+const ISLANDS_WITH_WEBP_AMBIENT_BACKGROUNDS = new Set([3]);
 
 export function getIslandBackgroundImageSrc(islandNumber: number): string {
-  return `/assets/islands/${getIslandArtFolderName(islandNumber)}/background/PLACEHOLDER__ambient-background.svg`;
+  const normalizedIslandNumber = normalizeIslandArtIslandNumber(islandNumber);
+  const backgroundFileName = ISLANDS_WITH_WEBP_AMBIENT_BACKGROUNDS.has(normalizedIslandNumber)
+    ? 'ambient-background.webp'
+    : 'PLACEHOLDER__ambient-background.svg';
+
+  return `/assets/islands/${getIslandArtFolderName(normalizedIslandNumber)}/background/${backgroundFileName}`;
 }
